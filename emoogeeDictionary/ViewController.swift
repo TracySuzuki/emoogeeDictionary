4cbb029ef3e2a4095f0736e06d71a160d67613e0
student15 <student15@student15s-air.ccc.edu>
2019-03-19 17:03:52 -0500

    Initial Commit

---
+++ emoogeeDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emoogeeDictionary
+//
+//  Created by student15 on 3/19/19.
+//  Copyright © 2019 student15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
